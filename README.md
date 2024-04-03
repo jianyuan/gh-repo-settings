a647cb81a9e67e40d28ab0892a5b94255675d6f0
Jian Yuan Lee <jianyuan@gmail.com>
2024-04-03 22:38:52 +0100

    feat: main tool

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 A tool that standardizes configurations, permissions, and settings across my GitHub projects.
 
+This tool uses `bun` and the GitHub CLI `gh` executable. You must already be authenticated via `gh auth login` before using the tool.
+
 To install dependencies:
 
 ```bash
--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-console.log("Hello via Bun!");
+import { $ } from "bun";
+
+const repos =
+  await $`gh repo list --no-archived --limit 1000 --json nameWithOwner --jq '[.[] | .nameWithOwner]`.json();
+
+console.log(`Found ${repos.length} repos`);
+
+for (const repo of repos) {
+  console.log(`Updating ${repo}`);
+  await $`gh repo edit ${repo} --allow-update-branch --delete-branch-on-merge --enable-merge-commit=false --enable-rebase-merge=false --enable-squash-merge`;
+}
+
+console.log("Done");
